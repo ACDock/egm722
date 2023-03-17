110726ee2cf09462e73f69286fb6fc8e35d92e5c
ACDock <127091143+ACDock@users.noreply.github.com>
2023-03-17 10:38:14 +0000

    keep track of number of guesses
    New feature added that keeps track of number of guesses - Lines 8 and 11.
    Prints out number of guesses - Line 20.

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -5,8 +5,10 @@ rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
+guess_number = 1
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
+    guess_number = guess_number + 1
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
     else:  # if the guess is too low, tell the user.
@@ -15,4 +17,4 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     print('Enter a new guess: ')
     guess = int(input())
 
-print('You got it! The number was {}'.format(rand))
+print('You got it! The number was {}! It took you {} tries'.format(rand, guess_number))
